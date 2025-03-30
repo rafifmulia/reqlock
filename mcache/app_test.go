fd5196dffbebd75be95148febb162ce6ec5bcd3f
rafifmulia <rafif.mulia.r@gmail.com>
2025-03-30 15:06:16 +0700

    fix import cycle

--- mcache/app_test.go
+++ mcache/app_test.go
@@ -3,9 +3,12 @@ package mcache
 import (
 	"sync"
 	"testing"
-
-	"github.com/rafifmulia/reqlock"
 )
+
+type Ticket struct {
+	Id, Room, Seat int32
+	Film           string
+}
 
 // go test -v -count=1 -failfast -cpu=4 -run='^TestSet1$'
 func TestSet1(t *testing.T) {
@@ -13,7 +16,7 @@ func TestSet1(t *testing.T) {
 		concurrentReq int32           = 40
 		wg            *sync.WaitGroup = &sync.WaitGroup{}
 	)
-	ticket := &reqlock.Ticket{Film: "batman", Room: 2, Seat: 7}
+	ticket := &Ticket{Film: "batman", Room: 2, Seat: 7}
 	Set("bookseat", ticket)
 	wg.Add(int(concurrentReq))
 	for i := int32(0); i < concurrentReq; i++ {
